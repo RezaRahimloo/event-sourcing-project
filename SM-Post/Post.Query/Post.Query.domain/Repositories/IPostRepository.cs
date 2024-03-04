d44d76cd7a28232d10644151e9d63f712b80751d
Reza Rahimlou <mr.rezarahimlou@gmail.com>
2024-03-04 11:28:01 +0330

    Added Read database Repository interface

---
+++ SM-Post/Post.Query/Post.Query.domain/Repositories/IPostRepository.cs
@@ -0,0 +1,21 @@
+﻿using Post.Query.domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Post.Query.domain.Repositories
+{
+    public interface IPostRepository
+    {
+        Task CreateAsync(PostEntity post);
+        Task UpdateAsync(PostEntity post);
+        Task DeleteAsync(Guid post);
+        Task<PostEntity> GetByIdAsync(Guid postId);
+        Task<List<PostEntity>> ListAllAsync();
+        Task<List<PostEntity>> ListByAutherAsync(string author);
+        Task<List<PostEntity>> ListwithLikesAsync(int numberOfLikes);
+        Task<List<PostEntity>> ListWithCommentsAsync();
+    }
+}
